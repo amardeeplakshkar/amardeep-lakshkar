b5b73ac3329e6e8d297cafea5f43c10e6026eef7
amardeeplakshkar <info.amardeeppvt@gmail.com>
2023-12-24 18:00:33 +0530

    fix text rotation in mobile browsers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,37 +7,33 @@ const Header = () => {
   };
   return (
     <>
-    <div className="social">
-          <ColorChangerButton />
-          <button onClick={() => handleButtonClick('https://twitter.com/amardeeplaksh04')} className="social-btn">
-            <i className="social-icon fa-brands fa-twitter" />
-          </button>
-          <button onClick={() => handleButtonClick('https://instagram.com/amardeep_lakshkar')} className="social-btn">
-            <i className="social-icon fa-brands fa-instagram" />
-          </button>
-          <button onClick={() => handleButtonClick('https://in.linkedin.com/in/amardeep-lakshkar-24a339244')} className="social-btn">
-            <i className="social-icon fa-brands fa-linkedin" />
-          </button>
-<<<<<<< HEAD
-          <button onClick={() => handleButtonClick('https://www.youtube.com/@AmardeepLakshkar')}  className="social-btn">
-=======
-          <button onClick={() => handleButtonClick('https://youtube.com/@AmardeepLakshkar')}  className="social-btn">
->>>>>>> 4b592e4 (fix text rotation in mobile browsers)
-            <i className="social-icon fa-brands fa-youtube" />
-          </button>
-          <button onClick={() => handleButtonClick('mailto:lakshkaramardeep@gmail.com')} className="social-btn">
-            <i className="social-icon fa-solid fa-envelope" />
-          </button>
+      <div className="social">
+        <ColorChangerButton />
+        <button onClick={() => handleButtonClick('https://twitter.com/amardeeplaksh04')} className="social-btn">
+          <i className="social-icon fa-brands fa-twitter" />
+        </button>
+        <button onClick={() => handleButtonClick('https://instagram.com/amardeep_lakshkar')} className="social-btn">
+          <i className="social-icon fa-brands fa-instagram" />
+        </button>
+        <button onClick={() => handleButtonClick('https://in.linkedin.com/in/amardeep-lakshkar-24a339244')} className="social-btn">
+          <i className="social-icon fa-brands fa-linkedin" />
+        </button>
+        <button onClick={() => handleButtonClick('https://youtube.com/@AmardeepLakshkar')} className="social-btn">
+          <i className="social-icon fa-brands fa-youtube" />
+        </button>
+        <button onClick={() => handleButtonClick('mailto:lakshkaramardeep@gmail.com')} className="social-btn">
+          <i className="social-icon fa-solid fa-envelope" />
+        </button>
+      </div>
+      <nav>
+        <div className='header'>
+          <a dir="rtl" href="#home" className="navlinks">Home</a>
+          <a dir="rtl" href="#about" className="navlinks">About</a>
+          <a dir="rtl" href="#skills" className="navlinks">Skils</a>
+          <a dir="rtl" href="#projects" className="navlinks">Projects</a>
+          <a dir="rtl" href="#contact" className="navlinks">Contact</a>
         </div>
-   <nav>
-   <div className='header'>
-       <a dir="rtl" href="#home" className="navlinks">Home</a>
-       <a dir="rtl" href="#about" className="navlinks">About</a>
-       <a dir="rtl" href="#skills" className="navlinks">Skils</a>
-       <a dir="rtl" href="#projects" className="navlinks">Projects</a>
-       <a dir="rtl" href="#contact" className="navlinks">Contact</a>
-    </div>
-   </nav>
+      </nav>
     </>
   )
 }
